747a61135d2c4a9e98f0342a8d07d4586f251d1f
Ronald Tse <ronald.tse@ribose.com>
2017-10-18 10:12:22 +0800

    Update README and download script

--- download.sh
+++ download.sh
@@ -3,8 +3,9 @@
 CHECKPATCH_SCRIPT=checkpatch.pl
 SPELLING_TXT=spelling.txt
 
-CHECKPATCH_DOWN_URL="https://raw.githubusercontent.com/torvalds/linux/master/scripts/$CHECKPATCH_SCRIPT"
-SPELLING_DOWN_URL="https://raw.githubusercontent.com/torvalds/linux/master/scripts/$SPELLING_TXT"
+KERNEL_RAW_URL="https://raw.githubusercontent.com/torvalds/linux/master"
+CHECKPATCH_URL="${KERNEL_RAW_URL}/scripts/${CHECKPATCH_SCRIPT}"
+SPELLING_URL="${KERNEL_RAW_URL}/scripts/${SPELLING_TXT}"
 
 # check if brew is installed
 readonly brew="/usr/local/bin/brew"
@@ -16,16 +17,16 @@ fi
 # check if wget is installed
 readonly WGET_DIR="$(${brew} --prefix wget)"
 if [ ! -d "${WGET_DIR}" ]; then
-	echo "Couldn't find wget installation directory. (Hint: 'brew install wget')"
+	echo "Couldn't find wget installation directory. (Hint: `brew install wget`)"
 	exit 1
 fi
 
 # remove the old files
-rm -f "$CHECKPATCH_SCRIPT" "$SPELLING_TXT"
+rm -f "${CHECKPATCH_SCRIPT}" "${SPELLING_TXT}"
 
 echo "Downloading checkpatch.pl..."
-wget "$CHECKPATCH_DOWN_URL"
-chmod 555 "$CHECKPATCH_SCRIPT"
+wget "${CHECKPATCH_URL}"
+chmod 555 "${CHECKPATCH_SCRIPT}"
 
 echo "Downloading spelling.txt..."
-wget "$SPELLING_DOWN_URL"
+wget "${SPELLING_URL}"
